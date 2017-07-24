3de48d960ec5fff9d13b23e62300530ecb3d442b
Patrick Uiterwijk <patrick@puiterwijk.org>
2017-07-24 03:13:45 +0200

    Do not remove older rpms
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ function cleanup {
     echo "Removing workdir"
     rm  -rf $WORKDIR
 }
-#trap cleanup EXIT
+trap cleanup EXIT
 
 echo "Workdir: $WORKDIR"
 
@@ -26,17 +26,10 @@ mkdir -p $WORKDIR/repo
     rpm --addsign *.rpm
 )
 
-# Create repo
-(
-    mv $WORKDIR/build/x86_64/* $WORKDIR/repo
-    cd $WORKDIR/repo
-    createrepo_c --database --xz .
-)
-
 # Move back here
 (
-    rm -rf repo/*
-    mv $WORKDIR/repo .
+    mv $WORKDIR/build/x86_64/*.rpm ./repo
+    createrepo_c --database --xz repo
     git add --all repo
     git commit -S -sm "Repository updated"
 )
